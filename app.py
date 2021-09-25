7a57154e4fd365413f17739a6a8745bc26aa6f73
inglisj <inglisj@tcd.ie>
2021-09-25 14:41:05 +0000

    add register html

--- app.py
+++ app.py
@@ -31,15 +31,14 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
-
 @app.route("/")
 @app.route("/add_recipe")
 def add_recipe():
      return render_template("add_recipe.html")
 
 
-@app.route("/registration", methods=["GET", "POST"])
-def registration():
+@app.route("/register", methods=["GET", "POST"])
+def register():
     if request.method == "POST":
        # check if username already exists in db
        existing_user = mongo.db.users.find_one(
@@ -49,18 +48,13 @@ def registration():
        flash("Username already exists")
        return redirect(url_for("register"))
 
-    register = {
+    new_user = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         } 
-mongo.db.users.insert_one(register)
-
-        # put the new user into 'session' cookie
-        session["user"] = request.form.get("username").lower()
-        flash("Registration Successful!")
-    return render_template("registration.html")
-
-    
+    mongo.db.users.insert_one(new_user)
+ 
+  
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
