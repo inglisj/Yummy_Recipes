091c0e25c4f03db088dbc4370d35723e2cc1122d
inglisj <inglisj@tcd.ie>
2021-09-27 14:09:05 +0000

    updated register page

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -16,7 +15,6 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
-
 
 @app.route("/")
 @app.route('/home_page')
@@ -44,16 +42,15 @@ def register():
        existing_user = mongo.db.users.find_one(
        {"username": request.form.get("username").lower()})
 
-def existing_user():
-    if existing_user:
-       flash("Username already exists")
-       return redirect(url_for("register"))
+       if existing_user():
+           flash("Username already exists")
+           return redirect(url_for("register"))
 
-    new_user = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        } 
-    mongo.db.users.insert_one(new_user)
+       register = {    
+           "username": request.form.get("username").lower(),
+           "password": generate_password_hash(request.form.get("password"))
+} 
+mongo.db.users.insert_one(register)
  
   
 if __name__ == "__main__":
